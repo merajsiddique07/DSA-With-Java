e224a8a55eb2d9f21606f541210b21588476fe44
Meraj Siddique <133602950+merajsiddique07@users.noreply.github.com>
2025-05-25 17:11:49 +0530

    Add files via upload

---
+++ Practice/P6.java
@@ -0,0 +1,11 @@
+class P6{
+    public static void main(String[] args) {
+        int a=1;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+            System.out.printf("%2d",a++);
+        }
+        System.out.println();
+        }
+    }
+}
---
+++ Practice/P7.java
@@ -0,0 +1,12 @@
+class P7{
+    public static void main(String[] args) {
+        int a=1;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+            System.out.printf("%2d",a);
+            a+=2;
+        }
+        System.out.println();
+        }
+    }
+}
